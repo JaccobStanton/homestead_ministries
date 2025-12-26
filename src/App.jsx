6ea9a856556dc655a4160f95152b2485f8897078
JaccobStanton <jaccobstanton@gmail.com>
2025-12-25 23:15:41 -0600

    Added routing to LionsManesLodge and PortabelloPlace components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import SiteLayout from "./layouts/SiteLayout";
 import Home from "./layouts/Home";
 import About from "./components/about/About";
 import AboutFrank from "./components/about/AboutFrank";
+import AccommodationsAll from "./components/accomodations/AccommodationsAll";
+import LionManesLodge from "./components/accomodations/LionManesLodge";
+import PortabelloPlace from "./components/accomodations/PortabelloPlace";
 // import About from "./pages/About";
 // import NotFound from "./pages/NotFound";
 
@@ -14,6 +17,15 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/about-frank" element={<AboutFrank />} />
+        <Route path="/accommodations" element={<AccommodationsAll />} />
+        <Route
+          path="/accommodations/lion-manes-lodge"
+          element={<LionManesLodge />}
+        />
+        <Route
+          path="/accommodations/portabello-place"
+          element={<PortabelloPlace />}
+        />
         {/* <Route path="*" element={<NotFound />} /> */}
       </Route>
     </Routes>
