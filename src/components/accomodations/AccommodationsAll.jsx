387de3b424c14fb22534e4b1ef4f56b97d3cf987
JaccobStanton <jaccobstanton@gmail.com>
2025-12-25 23:11:55 -0600

    Created AccomodationsAll component to show users the accomodations in column stacked order

---
+++ src/components/accomodations/AccommodationsAll.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Box } from "@mui/material";
+import LionManesLodge from "./LionManesLodge";
+import PortabelloPlace from "./PortabelloPlace";
+
+export default function AccommodationsAll() {
+  return (
+    <Box>
+      <LionManesLodge />
+      <PortabelloPlace />
+    </Box>
+  );
+}
