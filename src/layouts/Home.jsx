87a4f5081263152149a2da7459be954de369a419
JaccobStanton <jaccobstanton@gmail.com>
2025-12-25 19:55:58 -0600

    Drawer menu items in Navbar.jsx  now route to components in the Home.jsx page

--- src/layouts/Home.jsx
+++ src/layouts/Home.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Box } from "@mui/material";
 import Hero from "../components/hero/Hero";
 import AboutHomestead from "../components/about/AboutHomestead";
 import Accommodations from "../components/accomodations/Accomodations";
@@ -9,16 +10,30 @@ import ActivitiesGallery from "../components/activities/ActivitiesGallery";
 import Operation from "../components/operation/Operation";
 
 export default function Home() {
+  const sectionSx = { scrollMarginTop: { xs: 90, md: 120 } };
+
   return (
     <>
       <Hero />
-      <AboutHomestead />
-      <ThingsToDo />
-      <Accommodations />
+      <Box id="about" component="section" sx={sectionSx}>
+        <AboutHomestead />
+      </Box>
+      <Box id="things-to-do" component="section" sx={sectionSx}>
+        <ThingsToDo />
+      </Box>
+      <Box id="accommodations" component="section" sx={sectionSx}>
+        <Accommodations />
+      </Box>
       <Banner />
-      <Features />
-      <ActivitiesGallery />
-      <Operation />
+      <Box id="features" component="section" sx={sectionSx}>
+        <Features />
+      </Box>
+      <Box id="activities" component="section" sx={sectionSx}>
+        <ActivitiesGallery />
+      </Box>
+      <Box id="membership" component="section" sx={sectionSx}>
+        <Operation />
+      </Box>
     </>
   );
 }
